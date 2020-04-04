75c65fae7a4db76831c34fc3628778b8351553a6
Simon Aust <simon.aust@googlemail.com>
2020-04-04 11:29:52 +0100

    Stage 2/5

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
+    private static boolean auth = false;
 
     public static void main(String[] args) {
         play();
@@ -21,6 +22,10 @@ public class Main {
                 input = inSplit[0];
             }
             switch (input) {
+                case "auth":
+                    auth();
+                    input = sc.nextLine();
+                    break;
                 case "new":
                     newMusic();
                     input = sc.nextLine();
@@ -41,13 +46,30 @@ public class Main {
                     break;
                 default:
                     System.out.println("Unknown command. Try again.");
+                    input = sc.nextLine();
             }
 
         } while (!input.equals("exit"));
         System.out.println("---GOODBYE!---");
     }
 
+    private static void auth() {
+        System.out.println("https://accounts.spotify.com/authorize?client_id=6edb9b1ac21042abacc6daaf0fbc4c4d&redirect_uri=http://localhost:8080&response_type=code");
+        System.out.println("---SUCCESS---");
+        auth = true;
+    }
+
+    private static boolean checkAuth() {
+        if (!auth) {
+            System.out.println("Please, provide access for application.");
+        }
+        return auth;
+    }
+
     private static void newMusic() {
+        if (!checkAuth()) {
+            return;
+        }
         System.out.println("---NEW RELEASES---");
         System.out.printf("%s [%s, %s, %s]\n", "Mountains", "Sia", "Diplo", "Labrinth");
         System.out.printf("%s [%s]\n", "Runaway", "Lil Peep");
@@ -56,6 +78,9 @@ public class Main {
     }
 
     private static void featuredMusic() {
+        if (!checkAuth()) {
+            return;
+        }
         System.out.println("---FEATURED---");
         System.out.println("Mellow Morning");
         System.out.println("Wake Up and Smell the Coffee");
@@ -64,6 +89,9 @@ public class Main {
     }
 
     private static void categoriesMusic() {
+        if (!checkAuth()) {
+            return;
+        }
         System.out.println("---CATEGORIES---");
         System.out.println("Top Lists");
         System.out.println("Pop");
@@ -72,6 +100,9 @@ public class Main {
     }
 
     private static void playlistsMusic(String genre) {
+        if (!checkAuth()) {
+            return;
+        }
         System.out.printf("---%S PLAYLISTS---\n", genre);
         System.out.println("Walk Like A Badass");
         System.out.println("Rage Beats");
